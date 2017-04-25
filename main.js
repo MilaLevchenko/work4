17cc049457dfcbc57bd6b374fad2ddc358cf5300
Mila Levchenko <83darcey@mail.ru>
2017-04-25 20:19:57 +0300

    made header by js - add hamburger

--- main.js
+++ main.js
@@ -2,6 +2,35 @@
  * Created by Людмила on 24.04.2017.
  */
 $( document ).ready(function() {
+  var trigger = $('.hamburger');
+  var navSmall =  $('.nav-small');
+       isClosed = false;
+
+  trigger.click(function () {
+    hamburger_cross();
+  });
+
+  function hamburger_cross() {
+
+    if (isClosed == true) {
+      trigger.removeClass('is-open');
+      trigger.addClass('is-closed');
+      navSmall.removeClass('is-open');
+      navSmall.addClass('is-closed');
+      isClosed = false;
+    } else {
+      trigger.removeClass('is-closed');
+      trigger.addClass('is-open');
+      navSmall.removeClass('is-closed');
+      navSmall.addClass('is-open');
+      isClosed = true;
+    }
+  }
+
+
+
+
+
 
   $('.portfolio_item').hover(
       function(){
@@ -32,8 +61,6 @@ $( document ).ready(function() {
       }
     }
   })
-
-
 
 
 
