f443b80c4ce5773f31d3a8a3341ee859cf3bc845
Mila Levchenko <83darcey@mail.ru>
2017-04-25 17:42:17 +0300

    add team.html

--- main.js
+++ main.js
@@ -11,4 +11,33 @@ $( document ).ready(function() {
         $(this).find('.portfolio_item_dscr').slideUp(350); //.fadeOut(205)
       }
   );
+
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    dotsEach: true,
+    autoplay: true,
+    autoplayTimeout: 4000,
+    responsive:{
+      0:{
+        items:1
+      },
+      380:{
+        items:2
+      },
+      600:{
+        items:3
+      },
+      1000:{
+        items:5
+      }
+    }
+  })
+
+
+
+
+
 });
+
+
+
